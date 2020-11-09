92bc5999ed75853000d2e0e15cd649e77414a4e0
RyanLake6 <73082037+RyanLake6@users.noreply.github.com>
2020-11-08 21:19:26 -0500

    Update Pedal Potentiometers

--- ArduinoCode/Pedal_Potentiometers/Pedal_Potentiometers.ino
+++ ArduinoCode/Pedal_Potentiometers/Pedal_Potentiometers.ino
@@ -1,5 +1,3 @@
-#include <CAN.h>
-
 #include <mcp_can.h>
 #include <mcp_can_dfs.h>
 #include <SPI.h>
@@ -19,19 +17,20 @@ int brakePin1Val;
 int brakePin2Val;
  
 // CAN stuff
-const int CAN_ACCEL = 0x01;       // device's CAN id
-const int CAN_ACCEL_BROKE = 0x02; // device error ID
 const int CAN_BRAKE = 0x03;       // device's CAN id
-const int CAN_BRAKE_BROKE = 0x04; // device error ID
-const int CAN_MOTOR = 0x0;        //device's CAN id
+const int CAN_MOTOR = 0xC0;        // device's CAN id
 
 //Dashboard variables
 unsigned char len = 0;  //length of the data in the buffer
 unsigned char buf[1];  //received data (only 1 byte)
+bool invertorOn = false;
+bool isForward = true;
+
+// readPotentiometers() variables
+unsigned char forward[8] = {0,0,0,0,1,1,0,0}
+unsigned char reverse[8] = {0,0,0,0,0,1,0,0}
 
 const int SPI_CS_PIN = 10; // base CAN pin
-unsigned char errorMessage[8] = {0, 255, 0, 0, 0, 0, 0, 0}; // output message for when the potentiometers are too far apart
-// this message can be changed, but for now has value in second index as no "good" message will have a value there
 
 MCP_CAN CAN(SPI_CS_PIN); // setup can device
 
@@ -39,7 +38,7 @@ MCP_CAN CAN(SPI_CS_PIN); // setup can device
  * Start CAN and initialize digital pins
  */
 void setup() {
-  Serial.begin(115200); // baud rate for CAN
+  Serial.begin(115200); // Initializes Serial communication
 
   while (CAN_OK != CAN.begin(CAN_500KBPS, MCP_8MHz)) { // specify 8MHz crystal
     Serial.println("CAN BUS init Failed"); // failure message
@@ -71,10 +70,17 @@ void readPotentiometers() {
   if (abs(accelPin1Val - accelPin2Val) <= (1024 * 0.1)) {
     int averageReading = (accelPin1Val + accelPin2Val) / 2;
     unsigned char byteReading = averageReading / 4;
-    unsigned char valueMessage[8] = {byteReading, 0, 0, 0, 0, 0, 0, 0};
-    CAN.sendMsgBuf(CAN_ACCEL, 0, 8, valueMessage); // send message of the average value of the two potentiometers
-  } else {
-    CAN.sendMsgBuf(CAN_ACCEL_BROKE, 0, 8, errorMessage); // send message that the two potentiometer values are too far apart
+    if (isForward) {     // send motor potentiometer values when going forward
+      forward[0] = byteReading;
+      CAN.sendMsgBuf(CAN_MOTOR, 0, 8, forward);
+    }
+    else {    // send motor potentiometer values when in reverse
+      reverse[0] = byteReading;
+      CAN.sendMsgBuf(CAN_MOTOR, 0, 8, reverse);
+    }
+  } 
+  else {  //turn off motor
+    MotorOff();
   }
 }
 
@@ -86,11 +92,13 @@ void readSwitches() {
   brakePin1Val = digitalRead(brakePin1);
   brakePin2Val = digitalRead(brakePin2);
 
-  if ((brakePin1Val == LOW) && (brakePin2Val == LOW)) {
-    unsigned char valueMessage[8] = {1, 0, 0, 0, 0, 0, 0, 0};
-    CAN.sendMsgBuf(CAN_BRAKE, 0, 8, valueMessage); // send message that the switches are turned on
-  } else if (brakePin1Val != brakePin2Val) {
-    CAN.sendMsgBuf(CAN_BRAKE_BROKE, 0, 8, errorMessage); // send message that the two switch values are not equal
+  if ((brakePin1Val == LOW) || (brakePin2Val == LOW)) {  // switches are on
+    unsigned char valueMessage[1] = {1};
+    CAN.sendMsgBuf(CAN_BRAKE, 0, 1, valueMessage); // send message that the switches are turned on
+  } 
+  else {    // switches  are off
+    unsigned char valueMessage[1] = {0};
+    CAN.sendMsgBuf(CAN_BRAKE, 0, 1, valueMessage); // send message that the switches are turned off
   }
 }
 
@@ -99,20 +107,21 @@ void readDashboard() {
       CAN.readMsgBuf(&len, buf); //enters message into program
       canId = CAN.getCanId(); //gets sender ID
      if (canID == 0x01) {  //determine whether Dashboard is on or off
-      if (buf[0] == 0) {
-       // code for when dashboad is off
+      if (buf[0] == 0) {  // motor is off
+       invertorOn = false;
+       MotorOff();
       }
-      else if (buf[0] == 1) {
-       // code for when dashboard is on
+      else if (buf[0] == 1) {  //motor is on
+       invertorOn = true;
       }
      }
      
-     else if (canID == 0x02) {  //determine whether forward or revers
+     else if (canID == 0x02) {  //determine whether forward or reverse
       if (buf[0] == 0) {
-       // code for when in reverse
+       isForward = false;
       }
       else if (buf[0] == 1) {
-       // code for when forward
+       isForward = true;
       }
      }
     }
@@ -121,6 +130,11 @@ void readDashboard() {
 void MsgMotorController(int torque, int direction, int inverter) {  //torque must be 0-255, direction 0 is reverse, 1 is forward, inverter 0 is off, 1 is on
  unsigned char valueMessage[8] = {torque, 0, 0, direction, inverter, 0, 0, 0};
  CAN.sendMsgBuf(CAN_MOTOR, 0, 8, valueMessage);
+}
+
+void MotorOff() {
+  unsigned char valueMessage[8] = {0,0,0,0,0,0,0,0};
+  CAN.sendMsgBuf(CAN_MOTOR, 0, 8 valueMessage);
 }
 
  
