790c98fc256d39bb9a001b93bcf2d080f22c6b45
Joshua Cheng <josh.cheng.sh@gmail.com>
2020-11-10 12:29:11 -0500

    Updated with new CAN protocol, documentted.

--- ArduinoCode/Driver-IO/dashboard/dashboard.ino
+++ ArduinoCode/Driver-IO/dashboard/dashboard.ino
@@ -8,10 +8,10 @@
 const int spiCSPin = 10; // Base CAN pin
 MCP_CAN CAN(spiCSPin); // Set CS pin
 
-const unsigned char ON[1] = {1}; // Message for turning inverter on
-const unsigned char OFF[1] = {0}; // Message for turning inverter off
-const unsigned char FORWARD[1] = {1}; // Message for setting inverter to forward
-const unsigned char REVERSE[1] = {0}; // Message for setting inverter to reverse
+const unsigned char ON[] = {1}; // Message for turning inverter on
+const unsigned char OFF[] = {0}; // Message for turning inverter off
+const unsigned char FORWARD[] = {1}; // Message for setting inverter to forward
+const unsigned char REVERSE[] = {0}; // Message for setting inverter to reverse
 
 const int SHUTDOWN_DETECTION_PIN = 2; // Pin for detecting shutdown system voltage.
 const int BUTTON_PIN = 3; // Pin for on/off button.
@@ -25,9 +25,10 @@ const int BUTTON_LED_PIN = 11; // Pin for integrated LED on the button.
 const int SPEAKER_PIN = 10; // Pin for speaker or relay for speaker (must be pwm pin).
 
 bool shutdownVoltage = false;
-long speakerCooldown = 0;
-long buttonCooldown = 0;
+unsigned long speakerCooldown = 0; // Use unsigned long to match millis() function. Will overflow in around 50 days.
+unsigned long buttonCooldown = 0; // Use unsigned long to match millis() function. Will overflow in around 50 days.
 bool state = false;
+bool prevDirection = false;
 
 /**
     Sets relavent pins to output or input, initializes serial for debugging, initializes CAN.
@@ -48,30 +49,53 @@ void setup() {
     delay(250);
   }
   Serial.println("CAN BUS Shield Init OK!");
+}
 
+void imdDetection();
+
+void bmsDetection();
+
+bool forwardReverse();
+
+void onOff();
+
+void changeDireciton();
+
+void loop() {
+  imdDetection();
+  bmsDetection();
+  onOff();
+  changeDirection();
 }
 
 /**
-    On/off button with RTDS. IMD, BMS indicators. On/off, forward/reverse signals send to CAN
-
-    Will only set vehicle to "ready to drive" if shutdown circuit is enabled and button is pressed.
-    Button LED is activated when shutdown circuit is detected. Speaker will activate for 3 seconds when
-    the vehicle becomes "ready to drive".
+   Turns IMD indicator on or off depending on if IMD_INPUT_PIN detects a voltage.
 */
-void loop() {
-  // Turns IMD indicator on or off depending on if IMD_INPUT_PIN detects a voltage.
+void imdDetection() {
   if (digitalRead(IMD_INPUT_PIN)) {
     digitalWrite(IMD_INDICATOR_PIN, HIGH);
   } else {
     digitalWrite(IMD_INDICATOR_PIN, LOW);
   }
-  // Turns BMS indicator on or off depending on if BMS_INPUT_PIN detects a voltage.
+}
+
+/**
+   Turns BMS indicator on or off depending on if BMS_INPUT_PIN detects a voltage.
+*/
+void bmsDetection() {
   if (digitalRead(BMS_INPUT_PIN)) {
     digitalWrite(BMS_INDICATOR_PIN, HIGH);
   } else {
     digitalWrite(BMS_INDICATOR_PIN, LOW);
   }
+}
 
+/**
+   Use the shutdown circuit detection, on/off button, and gear selection pin to determine the car's state (on or off)
+   and direction. Car will only be ready to drive if shutdown circuit is detected. Speaker is turned on for 3 seconds
+   or until car is turned off (whichever comes first).
+*/
+void onOff() {
   shutdownVoltage = digitalRead(SHUTDOWN_DETECTION_PIN); // Detects if shutdown circuit is active (5v divided from 12v).
 
   // If shutdown circuit is active, turn on the LED. Otherwise, turn LED and speaker off, and set state to off
@@ -80,44 +104,45 @@ void loop() {
   } else {
     digitalWrite(BUTTON_LED_PIN, LOW);
     state = 0;
-    speakerCooldown = millis();
+    speakerCooldown = millis() + 1;
   }
 
   // Turns speaker off once cooldown is passed or state is false.
   if (speakerCooldown < millis() || !state) {
-    // Serial.println("Speaker Off");
     analogWrite(SPEAKER_PIN, 0);
   }
 
   /* If the shutdown circuit is active, button cooldown has passed, and button is pressed,
-     toggle state, reset button cooldown. If state is true, turn on speaker.
+     toggle state, reset button cooldown. If state is true, turn on speaker and tell pedal box
+     to turn on and the drive direction of the vehicle. If car was turned off, tell that to the pedal box.
   */
   if (shutdownVoltage && buttonCooldown < millis() && digitalRead(BUTTON_PIN) == LOW) {
     state = !state;
     buttonCooldown = millis() + 500; // Set cooldown to 500ms (prevents triggering button multiple times with one press).
-    if (state) {
+    if (state) { // Car is turned on.
       analogWrite(SPEAKER_PIN, 128);
-      // Serial.println("Speaker On");
       speakerCooldown = millis() + 3000;
+      CAN.sendMsgBuf(0x01, 0, 1, ON); // Tell pedal box to turn inverter on
+      if (digitalRead(GEAR_SELECTOR_PIN)) { // Send direction to pedal box when car is turned on.
+        CAN.sendMsgBuf(0x02, 0, 1, FORWARD);
+      } else {
+        CAN.sendMsgBuf(0x02, 0, 1, REVERSE);
+      }
+    } else { // Car is turned off.
+      CAN.sendMsgBuf(0x01, 0, 1, OFF); // tell pedal box to turn inverter off
     }
   }
+}
 
-  if (state) {
-    CAN.sendMsgBuf(0x01, 0, 1, ON); // tell pedal box to turn inverter on
-  } else {
-    CAN.sendMsgBuf(0x01, 0, 1, OFF); // tell pedal box to turn inverter off
+/**
+   Tells pedal box the forward/reverse direciton. Only sends message when there is a change.
+*/
+void changeDirection() {
+  if (prevDirection != digitalRead(GEAR_SELECTOR_PIN)) {
+    if (digitalRead(GEAR_SELECTOR_PIN)) {
+      CAN.sendMsgBuf(0x02, 0, 1, FORWARD); // tell pedal box to set inverter to forward
+    } else {
+      CAN.sendMsgBuf(0x02, 0, 1, REVERSE); // tell pedal box to set inverter to reverse
+    }
   }
-
-  if (digitalRead(GEAR_SELECTOR_PIN)) {
-    CAN.sendMsgBuf(0x02, 0, 1, FORWARD); // tell pedal box to set inverter to forward
-  } else {
-    CAN.sendMsgBuf(0x02, 0, 1, REVERSE); // tell pedal box to set inverter to reverse
-  }
-
-  // - - - - - - - - - - - - - - - Below is debug stuff - - - - - - - - - - - - - - - - - - - -
-//  String s = "off";
-//  if (state) {
-//    s = "on";
-//  }
-//  Serial.println("STATE: " + s + "  |  Shutdown Circuit Detection: " + String(shutdownVoltage));
 }
