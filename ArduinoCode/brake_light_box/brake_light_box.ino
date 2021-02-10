e73c2750e25ff46f591613a619565ab1b11c9fc1
Joshua Cheng <josh.cheng.sh@gmail.com>
2021-02-09 22:05:16 -0500

    Update brake_light_box.ino

--- ArduinoCode/brake_light_box/brake_light_box.ino
+++ ArduinoCode/brake_light_box/brake_light_box.ino
@@ -8,13 +8,16 @@
    CLK - pin 13
 
    SD card module:
-   SS - pin 10
+   SS - pin 4
 
    CAN module:
    SS - pin 9
 
    Note: This program keeps the CAN module activated by default, but deactivates it when writing to the SD card.
-   This Arduino may miss CAN messages if it recieves a message while writing to the SD card.
+   This Arduino may miss CAN messages if it recieves 2 or more messages while writing to the SD card. Also, when
+   the CAN module is used, strings longer than about 200-300 characters seem to be broken. It not only make the
+   string weird (cannot be appended to) as well as seems to break Serial.print() statements (only some parts of it
+   will actually print). I'm not sure if this is a problem with my code or a hardware issue. 
 */
 
 #include <mcp_can.h> // Uses Seeed-studio's CAN_BUS_Shield library.
@@ -22,9 +25,12 @@
 #include <SPI.h>
 #include <SD.h>
 
-#define SD_SS_PIN 10
-#define CAN_SS_PIN 9
+#define SD_SS_PIN 4
+#define CAN_SS_PIN 10
 #define BRAKE_LIGHT_PIN 6 // Does not have to be PWM pin.
+
+#define ERRORS_PER_WRITE 2 // Number of errors before writing the buffer to the SD card 
+                           // anything greater than 2 seems to break the program
 
 File myFile;
 MCP_CAN CAN(CAN_SS_PIN);
@@ -36,7 +42,7 @@ String errorBuffer = "";
 int bufferLength = 0;
 long lastLog = 0; // Time of last successful log to SD
 
-int nextFileName = 0; // Log file names in the format log-0.txt
+int nextFileName = 1; // Log file names in the format log-0.txt
 
 /**
    Sets relavent pins to output or input, initializes serial for debugging, initializes CAN, SD card reader,
@@ -66,30 +72,15 @@ void setup() {
     delay(250);
   }
 
-  if (!SD.exists("filenames.txt")) {
-    Serial.println("filenames.txt does not exist. Creating filenames.txt...");
-    myFile = SD.open("filenames.txt", FILE_WRITE);
-    myFile.println("0");
-    nextFileName = 1;
-    myFile.close();
-  } else {
-    myFile = SD.open("filenames.txt", FILE_READ); // filenames.txt contains the names of log files already written in the format 0\n1\n2\n etc.
-    while (myFile.available()) {
-      if (myFile.peek() == '\n') {
-        myFile.read();  // moves on to the next byte
-      } else {
-        nextFileName = (int)myFile.read() + 1; // At the end of the loop, fileName will be the last number in filenames.txt
-      }
-    }
-    myFile.close();
+  while(SD.exists("log-" + String(nextFileName) + ".txt")){
+    Serial.println("log-" + String(nextFileName) + ".txt exists...");
+    nextFileName += 1;
   }
-  myFile = SD.open("filenames.txt", FILE_WRITE);
-  myFile.println(String(nextFileName)); // record the file number being used this time
-  myFile.close();
+  
+  Serial.println("setup complete. nextFileName is " + String(nextFileName));
 
   digitalWrite(SD_SS_PIN, HIGH); // disable SD module
   digitalWrite(CAN_SS_PIN, LOW); // enable CAN module
-
 }
 
 bool SDWrite(String errors);
@@ -103,6 +94,12 @@ void loop() {
     CAN.readMsgBuf(&len, buf); //enters message into program
     canId = CAN.getCanId(); //gets sender ID
 
+    Serial.print("Message Recieved from CAN ID: " + String(canId) + "  |  message: ");
+    for(int i = 0; i < 8; i ++){
+      Serial.print(String(buf[i]) + " | ");
+    }
+    Serial.println();
+
     if (canId == 0x03) { // from pedal box
       digitalWrite(BRAKE_LIGHT_PIN, buf[0]); // either 0 or 1
     } else if (canId == 0x04) { // brake light error msg
@@ -111,17 +108,18 @@ void loop() {
       logError("0x05", buf);
     } else if (canId == 0x06) { // BMS DTC Status #1 and #2
       logError("0x06", buf);
-    } else if (canId == 0x0AB) { // Motor controller fault codes
-      logError("0x0AB", buf);
+    } else if (canId == 0xAB) { // Motor controller fault codes
+      logError("0xAB", buf);
     }
   }
 
   if (millis() - lastLog > 30000 && bufferLength > 0) { // if 30 seconds since last successful write to SD card and buffer is not empty
-    if (SDWrite(errorBuffer)) { // if successfully logged to SD card, clear buffer
+    if (SDWrite()) { // if successfully logged to SD card, clear buffer
       errorBuffer = "";
       bufferLength = 0;
     }
   }
+  
 }
 
 /**
@@ -131,19 +129,24 @@ void loop() {
    @param error the errors to be logged
 */
 void logError(String id, unsigned char error[8]) {
-  errorBuffer += "millis: " + String(millis()) + "  |  ";
-  errorBuffer += "id: " + id + "  |  error: | ";
+  errorBuffer += "millis: ";
+  errorBuffer += String(millis());
+  errorBuffer += "  | id: ";
+  errorBuffer += id;
+  errorBuffer += " |  error: | ";
   for (int i = 0; i < 8; i ++) {
     errorBuffer += String(int(error[i])) + " | ";  // stores the byte value (0-255) as a string
   }
   errorBuffer += "\n";
+  Serial.println(errorBuffer);
   bufferLength += 1;
-  if (bufferLength > 5) {
-    if (SDWrite(errorBuffer)) { // if successfully logged to SD card, clear buffer
+  if (bufferLength >= ERRORS_PER_WRITE) {
+    if (SDWrite()) { // if successfully logged to SD card, clear buffer
       errorBuffer = "";
       bufferLength = 0;
     }
   }
+  
 }
 
 /**
@@ -152,7 +155,7 @@ void logError(String id, unsigned char error[8]) {
    @param errors the error(s) to be logged
    @returns true if logging was successful, false if it failed
 */
-bool SDWrite(String errors) {
+bool SDWrite() {
   digitalWrite(CAN_SS_PIN, HIGH); // disable CAN module
   digitalWrite(SD_SS_PIN, LOW); // enable SD module
 
@@ -166,8 +169,9 @@ bool SDWrite(String errors) {
   myFile = SD.open("log-" + String(nextFileName) + ".txt", FILE_WRITE);
 
   if (myFile) {
-    myFile.println(errors);
+    myFile.println(errorBuffer);
     myFile.close();
+    Serial.println("Successfully logged " + String(errorBuffer.length()) + " characters at: log-" + String(nextFileName) + ".txt");
   } else {
     Serial.println("error opening: log-" + String(nextFileName) + ".txt");
     digitalWrite(SD_SS_PIN, HIGH); //disable SD module
