5b174ab2cf9ac82eb8f667979f526aa7897aa745
Peter Rydzynski <55663555+rydzynskip@users.noreply.github.com>
2021-03-10 23:39:52 -0500

    Reduce code duplication

--- ArduinoCode/Pedal_Box/Pedal_Box.ino
+++ ArduinoCode/Pedal_Box/Pedal_Box.ino
@@ -134,18 +134,11 @@ void readPotentiometers() {
   accelPin1Val = analogRead(ACCEL_PIN1); 
   accelPin2Val = analogRead(ACCEL_PIN2);
 
-  if(accelPin1Val == 0 || accelPin2Val == 0){
+  if(accelPin1Val == 0 || accelPin2Val == 0 || accelPin1Val == 1023 || accelPin2Val == 1023){
     CAN.sendMsgBuf(CAN_BMS_SHUTDOWN, 0, 8, BMS_ERROR);
     CAN.sendMsgBuf(CAN_MOTOR, 0, 8, MOTOR_OFF);
     CAN.sendMsgBuf(CAN_ACCEL_ERROR, 0, 4, ACCEL_ERROR);
   }
-
-  if(accelPin1Val == 1023 || accelPin2Val == 1023){
-    CAN.sendMsgBuf(CAN_BMS_SHUTDOWN, 0, 8, BMS_ERROR);
-    CAN.sendMsgBuf(CAN_MOTOR, 0, 8, MOTOR_OFF);
-    CAN.sendMsgBuf(CAN_ACCEL_ERROR, 0, 4, ACCEL_ERROR);
-  }
-
 
   if (abs(accelPin1Val - accelPin2Val) <= (1023 * 0.1)) {
     unsigned char accelTorque = 0;
