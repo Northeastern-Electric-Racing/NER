123fde8680c6b014b75cb00f6195dcd90c757604
mccauley.ma <mccauley.ma@husky.neu.edu>
2021-10-31 23:12:34 -0400

    Added 3rd cell and made improvments
    - Added third cell and accompanying calcs
    - Fine tuned calibration offset
    - Reduced serial lines
    - Formatted time stamp

--- ArduinoCode/CellVoltageMeasurement/CellVoltageMeasurement/CellVoltageMeasurement.ino
+++ ArduinoCode/CellVoltageMeasurement/CellVoltageMeasurement/CellVoltageMeasurement.ino
@@ -1,18 +1,23 @@
 float sense1;
 float sense2;
+float sense3;
 float voltage1;
 float voltage2;
+float voltage3;
+float t;
 float x;
-//const int ainput2 = 10;
+
 void setup()
 {
   Serial.begin(9600);
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
-  // pinMode(ainput2, INPUT);
+  pinMode(A2, INPUT);
   sense1 = 0;
   sense2 = 0;
+  sense3 = 0;
   x = 0;
+  Serial.println("Time\tVolt 1\tVolt 2\tVolt 3");
 }
 
 void loop()
@@ -20,22 +25,17 @@ void loop()
   // put your main code here, to run repeatedly:
   if (millis() >= (x))
   {
+    t = millis();
     sense1 = analogRead(A0);
     sense2 = analogRead(A1);
+    sense3 = analogRead(A1);
 
-    voltage1 = sense1 * (5.0 / 1024.0) + .1;
-    voltage2 = sense2 * (5.0 / 1024.0) + .1;
+    voltage1 = sense1 * (5.0 / 1024.0) + .14;
+    voltage2 = sense2 * (5.0 / 1024.0) + .14;
+    voltage3 = sense3 * (5.0 / 1024.0) + .14;
 
-    Serial.print(voltage1);
-    Serial.print("\t");
-    Serial.print(voltage2);
-    Serial.print("\t");
-    Serial.print(millis());
-    Serial.println("");
+    Serial.println(String(t / 1000, 0) + "\t" + String(voltage1) + "\t" + String(voltage2) + "\t" + String(voltage3));
 
     x += 1000;
   }
-
-
-  //delay(1000);
 }
