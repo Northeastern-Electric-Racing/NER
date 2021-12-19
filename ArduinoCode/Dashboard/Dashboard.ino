edf7c403cbc1209e4d9b6e407800f091e4f2e891
Peter Rydzynski <55663555+rydzynskip@users.noreply.github.com>
2021-12-19 17:17:31 -0500

    Update Dashboard Arduino Code (#106)

--- ArduinoCode/Dashboard/Dashboard.ino
+++ ArduinoCode/Dashboard/Dashboard.ino
@@ -3,6 +3,16 @@
 
 #include <mcp_can.h> // uses seeed-studio's CAN_BUS_Shield library
 
+
+#define CAN_FAULT_LED      0
+#define GENERAL_LED_2      1 // custom behavior 
+#define GENERAL_LED_1      2 // custom behavior
+#define ON_OFF_LED         3 // intergrated LED on the ON_OFF button
+#define ON_OFF_BUTTON      4
+#define SHUTDOWN_DETECTION 5 // shutdown circuit enable for if the car can run
+#define DIRECTION_SWITCH   8
+#define SPEAKER            9
+
 const int spiCSPin = 10; // Base CAN pin
 MCP_CAN CAN(spiCSPin); // Set CS pin
 
@@ -10,13 +20,6 @@ const unsigned char ON[] = {1}; // Message for turning inverter on
 const unsigned char OFF[] = {0}; // Message for turning inverter off
 const unsigned char FORWARD[] = {1}; // Message for setting inverter to forward
 const unsigned char REVERSE[] = {0}; // Message for setting inverter to reverse
-
-const int SHUTDOWN_DETECTION_PIN = 2; // Pin for detecting shutdown system voltage.
-const int BUTTON_PIN = 3; // Pin for on/off button.
-const int GEAR_SELECTOR_PIN = 4; // Pin for forward/reverse selector
-
-const int BUTTON_LED_PIN = 11; // Pin for integrated LED on the button.
-const int SPEAKER_PIN = 10; // Pin for speaker or relay for speaker (must be pwm pin).
 
 unsigned char len = 0; // length of incoming data
 unsigned char buf[8];  // received data
@@ -35,12 +38,15 @@ bool prechargeComplete = false;
 void setup() {
   Serial.begin(115200);
 
-  pinMode(BUTTON_PIN, INPUT_PULLUP); // Uses built-in pullup resistor.
-  pinMode(GEAR_SELECTOR_PIN, INPUT_PULLUP); // Uses built-in pullup resistor.
-
-  pinMode(SPEAKER_PIN, OUTPUT);
-  pinMode(BUTTON_LED_PIN, OUTPUT);
-
+  pinMode(CAN_FAULT_LED, OUTPUT); 
+  pinMode(GENERAL_LED_2, OUTPUT); 
+  pinMode(GENERAL_LED_1, OUTPUT); 
+  pinMode(ON_OFF_LED, OUTPUT); 
+  pinMode(ON_OFF_BUTTON, INPUT); 
+  pinMode(SHUTDOWN_DETECTION, INPUT); 
+  pinMode(DIRECTION_SWITCH, INPUT); 
+  pinMode(SPEAKER, OUTPUT);
+  
   while (CAN_OK != CAN.begin(CAN_250KBPS, MCP_8MHz)) { //specify 8MHz crystal
     Serial.println("CAN BUS init Failed");
     delay(250);
@@ -85,31 +91,31 @@ void checkMCStates() {
  * or until car is turned off (whichever comes first).
  */
 void onOff() {
-  shutdownVoltage = digitalRead(SHUTDOWN_DETECTION_PIN); // Detects if shutdown circuit is active (5v divided from 12v).
+  shutdownVoltage = digitalRead(SHUTDOWN_DETECTION); // Detects if shutdown circuit is active (5v divided from 12v).
 
   // If shutdown circuit is active, turn on the LED. Otherwise, turn LED and speaker off, and set state to off
   if (shutdownVoltage) {
-    digitalWrite(BUTTON_LED_PIN, HIGH);
+    digitalWrite(ON_OFF_LED, HIGH);
   } else {
-    digitalWrite(BUTTON_LED_PIN, LOW);
+    digitalWrite(ON_OFF_LED, LOW);
     state = 0;
     speakerCooldown = millis() + 1;
   }
 
   // Turns speaker off once cooldown is passed or state is false.
   if (speakerCooldown < millis() || !state) {
-    analogWrite(SPEAKER_PIN, 0);
+    analogWrite(SPEAKER, 0);
   }
 
   /* If the shutdown circuit is active, button cooldown has passed, and button is pressed,
    * toggle state, reset button cooldown. If state is true, turn on speaker and tell pedal box
    * to turn on and the drive direction of the vehicle. If car was turned off, tell that to the pedal box.
    */
-  if (shutdownVoltage && buttonCooldown < millis() && digitalRead(BUTTON_PIN) == LOW) {
+  if (shutdownVoltage && buttonCooldown < millis() && digitalRead(ON_OFF_BUTTON) == LOW) {
     state = !state;
     buttonCooldown = millis() + 500; // Set cooldown to 500ms (prevents triggering button multiple times with one press).
     if (state) { // Car is turned on.
-      analogWrite(SPEAKER_PIN, 128);
+      analogWrite(SPEAKER, 128);
       speakerCooldown = millis() + 3000;
       CAN.sendMsgBuf(0x101, 0, 1, ON); // Tell pedal box to turn inverter on
       changeDirection(true);
@@ -123,10 +129,10 @@ void onOff() {
  * Tells pedal box the forward/reverse direction. Only sends message when there is a change or when car is just turned on.
  */
 void changeDirection(bool justOn) {
-  if (!justOn && prevDirection == digitalRead(GEAR_SELECTOR_PIN)) {
+  if (!justOn && prevDirection == digitalRead(DIRECTION_SWITCH)) {
     return;
   }
-  if (digitalRead(GEAR_SELECTOR_PIN)) {
+  if (digitalRead(DIRECTION_SWITCH)) {
     CAN.sendMsgBuf(0x102, 0, 1, FORWARD); // tell pedal box to set inverter to forward
     prevDirection = true;
   } else {
