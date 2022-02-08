1060ba531ca8bc43beaff8e0597c1236fd2dc927
Vatsal Mehta <vatsalbmehta3@gmail.com>
2022-02-07 19:34:58 -0500

    Added InfluxDB configurations

--- Telemetry/data-processing/constants.py
+++ Telemetry/data-processing/constants.py
@@ -1,6 +1,4 @@
-# Other option is to place these as environment variables to protect privacy
-
-BUCKET = ""
-ORG = ""
-TOKEN = ""
-URL = ""
+BUCKET = "Test"
+ORG = "f9e80fbc8c88e694"
+TOKEN = "r9hsS4oPtzVxvUyrprEfcWUaPxPZ-L9RPU_JbKrUbCkYPfhGedGiYx96BTZNUO4ZNy9O-unHbOZ1PpDmjGCz3Q=="
+URL = "https://us-east-1-1.aws.cloud2.influxdata.com/"
