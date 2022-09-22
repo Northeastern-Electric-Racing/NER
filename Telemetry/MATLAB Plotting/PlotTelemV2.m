590055f8eca4ee6c63cad30c5246d72924ac30d6
Matthew McCauley <60201095+mccauleyma@users.noreply.github.com>
2022-09-21 22:53:59 -0400

    Matt matlab code 2 (#133)
    * V2 script + git ignore
    
    New script that works with the new format of the telemetry hub CSV output while we wait for hub graphing improvements
    - also added .csv to git ignore
    
    * V3 code
    
    Uses the synchronize function to allow us to set exactly how many samples we want from the data. Runs much faster, although there are more optimizations that could be made if needed
    
    * Updated for peters new IDs
    
    * Fixed polarity
    
    Telem hub now does the reversals, so we dont need to

--- Telemetry/MATLAB Plotting/PlotTelemV2.m
+++ Telemetry/MATLAB Plotting/PlotTelemV2.m
@@ -3,29 +3,28 @@ close all;
 
 motor2roadConst = 0.01842102362; % Divide by diff ratio, multiply by wheel circumfrence, convert meters per min to MPH
 
-messageIDs = [165,165,1,514,166,1,2,162,192]; % CAN message ID
-dataInMessage = [4,4,5,2,4,5,6,4,7]; % How much data is in that message
-dataIndex = [2,2,2,1,4,1,5,3,1]; % Index of which data we want out of the message
-dataMulti = [-1,-motor2roadConst,0.1,1/256,1,0.1,1,1,1]; % Data multiplier
+dataIDs = [31,31,2,60,37,1,10,11,9]; % CAN message ID
+thinner = [0,0,0,0,0,0,0,1,0];
+dataMulti = [-1,-motor2roadConst,0.1,1,1,0.1,1,1,1]; % Data multiplier
 dataOffset = [0,0,0,0,0,0,0,0,0]; % Data +/- offset
-dataName = ["Motor Speed", "Wheel Speed", "BMS Pack Current", "Discharge Current Limit", "MC DC Current", "Pack Inst Voltage", "Average Temp", "Motor Temperature", "Commanded Torque"];
-plotOrder = {'Motor Speed', 'Wheel Speed', {'BMS Pack Current', 'Discharge Current Limit', 'MC DC Current'}, 'Pack Inst Voltage', {'Average Temp', 'Motor Temperature'}, 'Commanded Torque'};
+dataName = ["Motor Speed", "Wheel Speed", "BMS Pack Current", "Discharge Current Limit", "MC DC Current", "Pack Inst Voltage", "Average Temp", "Motor Temperature", "Limit Status"];
+plotOrder = {'Motor Speed', 'Wheel Speed', {'BMS Pack Current', 'Discharge Current Limit', 'MC DC Current'}, 'Pack Inst Voltage', {'Average Temp', 'Motor Temperature'}, 'Limit Status'};
 
-dataQty = length(messageIDs);
+dataQty = length(dataIDs);
 
 disp('Importing Data');
-T = readtable('AccelTest1.csv', 'Delimiter', ','); % SELECT FILE HERE
+T = readtable('091422_2359.csv', 'Delimiter', ','); % SELECT FILE HERE
 h = height(T);
 cell = T{:,2};
 disp('Import Complete');
 
 for i = 1:dataQty
 	fprintf('Processing data selection %2u of %2u\n', i, dataQty);
-	ind = cell == messageIDs(i); % Find message we want
-	dataT = T(ind,:); % Keep only message we want
-	dataH = height(dataT); % How many are there
-	ind = dataIndex(i):dataInMessage(i):dataH; % Create array selecting data (since it is always the same order)
-	dataT = dataT(ind,:); % Keep only data we want
+	ind = cell == dataIDs(i); % Find data we want
+	if thinner(i)
+		ind = ind(5:5:end);
+	end
+	dataT = T(ind,:); % Keep only data we want
 	dataT = timetable(datetime(table2array(dataT(:,1)), 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss.SSS''Z'),(dataT{:,4} * dataMulti(i) + dataOffset(i))); % Create a timetable and convert strings to datetimes
 	if i > 1 % Typically, join
 		procT = outerjoin(procT, dataT);
@@ -39,7 +38,7 @@ procT.Properties.VariableNames = dataName;
 temp = (table2array(procT(:,3)) > 1000);
 procT{temp,3} = -99;
 
-range = timerange('2022-07-31 20:05:00', '2022-07-31 20:34:00'); % SELECT TIME RANGE HERE
+range = timerange('2022-09-14 22:31:00', '2022-09-14 23:02:00'); % SELECT TIME RANGE HERE
 plotT = procT(range,:); % Keep only time we want
 
 % plotT(:,2:length(dataName)) = fillmissing(plotT(:,2:length(dataName)),'previous'); % Interpolate missing values (this just uses latest value)
@@ -48,4 +47,8 @@ for i = 1:dataQty
 	plotT{:,i} = fillmissing(plotT{:,i},'previous');
 end
 
+% thinning
+plotT = plotT(1:10:height(plotT),:);
+
+disp('Generating Plot!');
 s = stackedplot(plotT, plotOrder);
---
+++ Telemetry/MATLAB Plotting/PlotTelemV3.m
@@ -0,0 +1,39 @@
+clear;
+close all;
+
+motor2roadConst = 0.01842102362; % Divide by diff ratio, multiply by wheel circumfrence, convert meters per min to MPH
+
+dataIDs = [45,45,2,89,51,1,10,28,91,92,93]; % CAN message ID
+dataMulti = [1,motor2roadConst,0.1,1,1,0.1,1,1,1,1,1]; % Data multiplier
+dataOffset = [0,0,0,0,0,0,0,0,0,0,0]; % Data +/- offset
+dataName = ["Motor Speed", "Wheel Speed", "BMS Pack Current", "Discharge Current Limit", "MC DC Current", "Pack Inst Voltage", "Average Temp", "Motor Temperature", "Accel X", "Accel Y", "Accel Z"];
+plotOrder = {'Motor Speed', 'Wheel Speed', {'BMS Pack Current', 'Discharge Current Limit', 'MC DC Current'}, 'Pack Inst Voltage', {'Average Temp', 'Motor Temperature'}, {'Accel X', 'Accel Y', 'Accel Z'}};
+
+dataQty = length(dataIDs);
+
+disp('Importing Data');
+T = readtable('091822_2154.csv', 'Delimiter', ','); % SELECT FILE HERE
+tv = [datetime(2019,1,1,0,29,0):milliseconds(5):datetime(2019,1,1,0,59,0)]; % SELECT TIME RANGE HERE
+h = height(T);
+cell = T{:,2};
+disp('Import Complete');
+
+for i = 1:dataQty
+	fprintf('Processing data selection %2u of %2u\n', i, dataQty);
+	ind = cell == dataIDs(i); % Find data we want
+	dataT = T(ind,:); % Keep only data we want
+	dataT = timetable(datetime(table2array(dataT(:,1)), 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss.SSS''Z'),(dataT{:,4} * dataMulti(i) + dataOffset(i))); % Create a timetable and convert strings to datetimes
+	if i > 1 % Typically, join
+		procT = synchronize(procT, dataT, tv, 'previous');
+	else % If it's the first (or only), just define the new table
+		procT = dataT;
+	end
+end
+procT.Properties.VariableNames = dataName;
+
+% TEMP Patch for BMS current
+temp = (table2array(procT(:,3)) > 1000);
+procT{temp,3} = -99;
+
+disp('Generating Plot!');
+s = stackedplot(procT, plotOrder);
